dbd576161a6bb703a71b470d4e882741669017c0
Gary Wang <wzc782970009@gmail.com>
2020-06-04 23:54:34 +0800

    feat: basic i18n support

---
+++ languages/pineapple-music.ts
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Mono</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Stereo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>%1 Channels</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Select songs to play</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Audio Files</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <source>File list.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ languages/pineapple-music_zh_CN.ts
@@ -0,0 +1,54 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Mono</source>
+        <translation>单声道</translation>
+    </message>
+    <message>
+        <source>Stereo</source>
+        <translation>立体声</translation>
+    </message>
+    <message>
+        <source>%1 Channels</source>
+        <translation>%1 声道</translation>
+    </message>
+    <message>
+        <source>Select songs to play</source>
+        <translation>选择要播放的曲目</translation>
+    </message>
+    <message>
+        <source>Audio Files</source>
+        <translation>音频文件</translation>
+    </message>
+    <message>
+        <source>Pineapple Player</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>^</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No song loaded...</source>
+        <translation>未加载曲目...</translation>
+    </message>
+    <message>
+        <source>Drag and drop file to load</source>
+        <translation>拖放文件来播放</translation>
+    </message>
+    <message>
+        <source>0:00</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <source>File list.</source>
+        <translation>文件列表</translation>
+    </message>
+</context>
+</TS>
