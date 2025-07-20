094a83512e4cb4305eb81508cd7fdc5874a3242b
Gary Wang <git@blumia.net>
2025-07-20 19:05:02 +0800

    i18n: update ts files

--- languages/pineapple-music_en.ts
+++ languages/pineapple-music_en.ts
@@ -12,82 +12,113 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../mainwindow.cpp" line="93"/>
+        <location filename="../mainwindow.cpp" line="125"/>
         <source>Mono</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="95"/>
+        <location filename="../mainwindow.cpp" line="127"/>
         <source>Stereo</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="97"/>
+        <location filename="../mainwindow.cpp" line="129"/>
         <source>%1 Channels</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="241"/>
+        <location filename="../mainwindow.cpp" line="298"/>
         <source>Select songs to play</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="243"/>
+        <location filename="../mainwindow.cpp" line="300"/>
         <source>Audio Files</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="576"/>
+        <location filename="../mainwindow.cpp" line="685"/>
         <source>Select image as background skin</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="578"/>
+        <location filename="../mainwindow.cpp" line="687"/>
         <source>Image files (*.jpg *.jpeg *.png *.gif)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../mainwindow.cpp" line="764"/>
+        <source>Based on the following free software libraries:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
         <location filename="../mainwindow.ui" line="23"/>
+        <location filename="../mainwindow.cpp" line="762"/>
         <location filename="../lrcbar.cpp" line="89"/>
         <source>Pineapple Music</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="313"/>
+        <location filename="../mainwindow.ui" line="327"/>
         <source>No song loaded...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="325"/>
+        <location filename="../mainwindow.ui" line="339"/>
         <source>Drag and drop file to load</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="338"/>
+        <location filename="../mainwindow.ui" line="352"/>
         <source>Lrc</source>
         <comment>Lyrics</comment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="103"/>
+        <location filename="../mainwindow.ui" line="711"/>
+        <location filename="../mainwindow.ui" line="714"/>
+        <location filename="../mainwindow.cpp" line="759"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="728"/>
+        <source>Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="135"/>
         <source>Sample Rate: %1 Hz</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="108"/>
+        <location filename="../mainwindow.cpp" line="140"/>
         <source>Bitrate: %1 Kbps</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="113"/>
+        <location filename="../mainwindow.cpp" line="145"/>
         <source>Channel Count: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PlaybackProgressIndicator</name>
+    <message>
+        <location filename="../playbackprogressindicator.cpp" line="85"/>
+        <source>Time</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../playbackprogressindicator.cpp" line="85"/>
+        <source>Chapter Name</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>main</name>
     <message>
-        <location filename="../main.cpp" line="28"/>
+        <location filename="../main.cpp" line="27"/>
         <source>File list.</source>
         <translation type="unfinished"></translation>
     </message>
--- languages/pineapple-music_zh_CN.ts
+++ languages/pineapple-music_zh_CN.ts
@@ -12,82 +12,113 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../mainwindow.cpp" line="93"/>
+        <location filename="../mainwindow.cpp" line="125"/>
         <source>Mono</source>
         <translation>单声道</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="95"/>
+        <location filename="../mainwindow.cpp" line="127"/>
         <source>Stereo</source>
         <translation>立体声</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="97"/>
+        <location filename="../mainwindow.cpp" line="129"/>
         <source>%1 Channels</source>
         <translation>%1 声道</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="241"/>
+        <location filename="../mainwindow.cpp" line="298"/>
         <source>Select songs to play</source>
         <translation>选择要播放的曲目</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="243"/>
+        <location filename="../mainwindow.cpp" line="300"/>
         <source>Audio Files</source>
         <translation>音频文件</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="576"/>
+        <location filename="../mainwindow.cpp" line="685"/>
         <source>Select image as background skin</source>
         <translation>选择图片作为背景皮肤</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="578"/>
+        <location filename="../mainwindow.cpp" line="687"/>
         <source>Image files (*.jpg *.jpeg *.png *.gif)</source>
         <translation>图片文件 (*.jpg *.jpeg *.png *.gif)</translation>
     </message>
     <message>
+        <location filename="../mainwindow.cpp" line="764"/>
+        <source>Based on the following free software libraries:</source>
+        <translation>基于下列自由软件库：</translation>
+    </message>
+    <message>
         <location filename="../mainwindow.ui" line="23"/>
+        <location filename="../mainwindow.cpp" line="762"/>
         <location filename="../lrcbar.cpp" line="89"/>
         <source>Pineapple Music</source>
         <translation>菠萝音乐</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="313"/>
+        <location filename="../mainwindow.ui" line="327"/>
         <source>No song loaded...</source>
         <translation>未加载曲目...</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="325"/>
+        <location filename="../mainwindow.ui" line="339"/>
         <source>Drag and drop file to load</source>
         <translation>拖放文件来播放</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="338"/>
+        <location filename="../mainwindow.ui" line="352"/>
         <source>Lrc</source>
         <comment>Lyrics</comment>
         <translation>歌词</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="103"/>
+        <location filename="../mainwindow.ui" line="711"/>
+        <location filename="../mainwindow.ui" line="714"/>
+        <location filename="../mainwindow.cpp" line="759"/>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="728"/>
+        <source>Open</source>
+        <translation>打开</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="135"/>
         <source>Sample Rate: %1 Hz</source>
         <translation>采样率: %1 Hz</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="108"/>
+        <location filename="../mainwindow.cpp" line="140"/>
         <source>Bitrate: %1 Kbps</source>
         <translation>比特率: %1 Kbps</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="113"/>
+        <location filename="../mainwindow.cpp" line="145"/>
         <source>Channel Count: %1</source>
         <translation>声道数: %1</translation>
     </message>
 </context>
 <context>
+    <name>PlaybackProgressIndicator</name>
+    <message>
+        <location filename="../playbackprogressindicator.cpp" line="85"/>
+        <source>Time</source>
+        <translation>时间</translation>
+    </message>
+    <message>
+        <location filename="../playbackprogressindicator.cpp" line="85"/>
+        <source>Chapter Name</source>
+        <translation>章节名称</translation>
+    </message>
+</context>
+<context>
     <name>main</name>
     <message>
-        <location filename="../main.cpp" line="28"/>
+        <location filename="../main.cpp" line="27"/>
         <source>File list.</source>
         <translation>文件列表。</translation>
     </message>
