c6771e67471f4f3b881385162085ae8741716a44
Codeberg Translate <translate@codeberg.org>
2025-10-08 09:09:12 +0000

    i18n: Translations update from Codeberg Translate
    Co-authored-by: Codeberg Translate <translate@codeberg.org>
    Co-authored-by: Languages add-on <noreply-addon-languages@weblate.org>
    Co-authored-by: Vistaus <vistaus@noreply.codeberg.org>
    Translate-URL: https://translate.codeberg.org/projects/pineapple-apps/pineapple-music/nl/
    Translation: Pineapple Applications/Pineapple Music

---
+++ languages/pineapple-music_de.ts
@@ -0,0 +1,126 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de">
+<context>
+    <name>LrcBar</name>
+    <message>
+        <location filename="../lrcbar.cpp" line="88"/>
+        <source>(Interlude...)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="125"/>
+        <source>Mono</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="127"/>
+        <source>Stereo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="129"/>
+        <source>%1 Channels</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="298"/>
+        <source>Select songs to play</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="300"/>
+        <source>Audio Files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="685"/>
+        <source>Select image as background skin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="687"/>
+        <source>Image files (*.jpg *.jpeg *.png *.gif)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="764"/>
+        <source>Based on the following free software libraries:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="23"/>
+        <location filename="../mainwindow.cpp" line="762"/>
+        <location filename="../lrcbar.cpp" line="89"/>
+        <source>Pineapple Music</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="327"/>
+        <source>No song loaded...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="339"/>
+        <source>Drag and drop file to load</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="352"/>
+        <source>Lrc</source>
+        <comment>Lyrics</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="711"/>
+        <location filename="../mainwindow.ui" line="714"/>
+        <location filename="../mainwindow.cpp" line="759"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="728"/>
+        <source>Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="135"/>
+        <source>Sample Rate: %1 Hz</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="140"/>
+        <source>Bitrate: %1 Kbps</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="145"/>
+        <source>Channel Count: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PlaybackProgressIndicator</name>
+    <message>
+        <location filename="../playbackprogressindicator.cpp" line="85"/>
+        <source>Time</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../playbackprogressindicator.cpp" line="85"/>
+        <source>Chapter Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.cpp" line="27"/>
+        <source>File list.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ languages/pineapple-music_nl.ts
@@ -0,0 +1,126 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl">
+<context>
+    <name>LrcBar</name>
+    <message>
+        <location filename="../lrcbar.cpp" line="88"/>
+        <source>(Interlude...)</source>
+        <translation>(Intermezzo…)</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="125"/>
+        <source>Mono</source>
+        <translation>Mono</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="127"/>
+        <source>Stereo</source>
+        <translation>Stereo</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="129"/>
+        <source>%1 Channels</source>
+        <translation>%1 kanalen</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="298"/>
+        <source>Select songs to play</source>
+        <translation>Kies de af te spelen nummers</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="300"/>
+        <source>Audio Files</source>
+        <translation>Audiobestanden</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="685"/>
+        <source>Select image as background skin</source>
+        <translation>Kies een achtergrondafbeelding</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="687"/>
+        <source>Image files (*.jpg *.jpeg *.png *.gif)</source>
+        <translation>Afbeeldingsbestanden (*.jpg *.jpeg *.png *.gif)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="764"/>
+        <source>Based on the following free software libraries:</source>
+        <translation>Gebaseerd op deze vrijesoftwarebibliotheken:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="23"/>
+        <location filename="../mainwindow.cpp" line="762"/>
+        <location filename="../lrcbar.cpp" line="89"/>
+        <source>Pineapple Music</source>
+        <translation>Pineapple-muziekspeler</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="327"/>
+        <source>No song loaded...</source>
+        <translation>Geen nummer geladen…</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="339"/>
+        <source>Drag and drop file to load</source>
+        <translation>Sleep een bestand hierheen</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="352"/>
+        <source>Lrc</source>
+        <comment>Lyrics</comment>
+        <translation>Lrc</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="711"/>
+        <location filename="../mainwindow.ui" line="714"/>
+        <location filename="../mainwindow.cpp" line="759"/>
+        <source>About</source>
+        <translation>Over</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="728"/>
+        <source>Open</source>
+        <translation>Openen</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="135"/>
+        <source>Sample Rate: %1 Hz</source>
+        <translation>Samplesnelheid: %1 Hz</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="140"/>
+        <source>Bitrate: %1 Kbps</source>
+        <translation>Bitsnelheid: %1 Kbps</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="145"/>
+        <source>Channel Count: %1</source>
+        <translation>Aantal kanalen: %1</translation>
+    </message>
+</context>
+<context>
+    <name>PlaybackProgressIndicator</name>
+    <message>
+        <location filename="../playbackprogressindicator.cpp" line="85"/>
+        <source>Time</source>
+        <translation>Tijdstip</translation>
+    </message>
+    <message>
+        <location filename="../playbackprogressindicator.cpp" line="85"/>
+        <source>Chapter Name</source>
+        <translation>Hoofdstuknaam</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.cpp" line="27"/>
+        <source>File list.</source>
+        <translation>Bestandslijst.</translation>
+    </message>
+</context>
+</TS>
